ca5b9fabae0af8f663e7c17429b8eeabfd6570cd
Alireza Bashiri <azbshiri@gmail.com>
2019-09-15 03:04:29 +0700

    Change background color back to original

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Body(props) {
   const style = {
     ...props.style,
-    background: "#020611",
+    background: "#020631",
     display: "flow-root",
     height: "100vw",
     font: "14px 'Nunito', sans-serif"
