c7157ca5ee713c25a7f2b41d92df374645da38b2
Alireza Bashiri <azbshiri@gmail.com>
2019-09-15 05:43:00 +0700

    Add removing todo functionality
    Pulled up removing/adding functionality from TodoListItem to
    AddTodoItem.

--- src/components/AddTodoItem.js
+++ src/components/AddTodoItem.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import AddButton from "./AddButton";
 import TodoList from "./TodoList";
-import TodoListItem from "./TodoListItem";
 
 export default function AddTodoItem(props) {
   const [todos, setTodo] = useState([]);
@@ -27,14 +26,38 @@ export default function AddTodoItem(props) {
     el.value = "";
   }
 
+  function onCheck(e) {
+    const li = e.target.parentElement.parentElement.parentElement;
+    if (li.className.includes("danger")) {
+      li.className = "";
+      return;
+    }
+    li.className = "danger";
+  }
+
+  function onDelete(i) {
+    setTodo(todos.filter((todo, j) => i !== j));
+  }
+
   return (
     <div className="add-control" style={style}>
       <div className="form-group has-feedback">
         <TodoItem onKeyPress={onClick} />
         <AddButton onClick={onClick} />
         <TodoList>
-          {todos.map(todo => (
-            <TodoListItem>{todo.name}</TodoListItem>
+          {todos.map((todo, i) => (
+            <li key={i}>
+              <div className="checkbox">
+                <span className="close" onClick={() => onDelete(i)}>
+                  <i className="fa fa-times"></i>
+                </span>
+                <label>
+                  <span className="checkbox-mask"></span>
+                  <input onClick={onCheck} type="checkbox" />
+                  {todo.name}
+                </label>
+              </div>
+            </li>
           ))}
         </TodoList>
       </div>
